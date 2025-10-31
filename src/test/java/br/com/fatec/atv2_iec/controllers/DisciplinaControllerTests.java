58620363d49d6b9fa4c9c8ed572a5226295277da
Amanda <amanda.fralve@gmail.com>
2025-10-31 14:39:42 -0300

    test

--- src/test/java/br/com/fatec/atv2_iec/controllers/DisciplinaControllerTests.java
+++ src/test/java/br/com/fatec/atv2_iec/controllers/DisciplinaControllerTests.java
@@ -20,38 +20,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc
 public class DisciplinaControllerTests {
     @Autowired
-	private MockMvc mockMvc;
-
-	@Test
-	void deveRetornarListaDeDisciplinas() throws Exception {
-		// Lista de strings é convertida pelo Spring para JSON automaticamente
-		String jsonEsperado = "[\"Laboratório de Desenvolvimento WEB\",\"Estatística\",\"Programação para Dispositivos Móveis\"]";
-
-		mockMvc.perform(get("/disciplinas"))
-			.andExpect(status().isOk())
-			.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-
-			.andExpect(content().string(jsonEsperado));
-	}
+    private MockMvc mockMvc;
 
     @Test
-    void deveRetornarDisciplinaId() throws Exception{
+    void deveRetornarListaDeDisciplinas() throws Exception {
+        mockMvc.perform(get("/disciplinas"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(content().json("{\"1\":\"Laboratório de Desenvolvimento WEB\",\"2\":\"Estatística\",\"3\":\"Programação para Dispositivos Móveis\"}"));
+    }
+
+    @Test
+    void deveRetornarDisciplinaId() throws Exception {
         mockMvc.perform(get("/disciplinas/1"))
-            .andExpect((status().isOk()))
-            .andExpect(content().string("Laboratório de Desenvolvimento WEB"));
+            .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(content().json("{\"id\":1,\"nome\":\"Laboratório de Desenvolvimento WEB\"}"));
     }
 
     @Test
     void deveAdicionarNovaDisciplina() throws Exception {
-    Map<String, String> novaDisciplina = Map.of("codigo", "4", "nome", "Banco de Dados Relacional");
-    String json = new ObjectMapper().writeValueAsString(novaDisciplina);
+        Map<String, String> novaDisciplina = Map.of("codigo", "4", "nome", "Banco de Dados Relacional");
+        String json = new ObjectMapper().writeValueAsString(novaDisciplina);
 
-    mockMvc.perform(post("/disciplinas")
-            .contentType(MediaType.APPLICATION_JSON)
-            .accept(MediaType.APPLICATION_JSON)
-            .content(json))
-        .andExpect(status().isOk())
-        .andExpect(content().string("Disciplina adicionada"));
+        mockMvc.perform(post("/disciplinas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(json))
+            .andExpect(status().isCreated())
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(content().json("{\"id\":4,\"nome\":\"Banco de Dados Relacional\",\"message\":\"Disciplina adicionada\"}"));
+    }
 }
 
-}
+
